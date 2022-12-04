f262d353b62c461f1871603ac25993946f7c5c50
am_utkin <am_utkin@controlsystems.ru>
2022-12-04 20:03:56 +0300

    swagger comments

--- src/links/dto/create-link.dto.ts
+++ src/links/dto/create-link.dto.ts
@@ -1,6 +1,11 @@
 import { IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateLinkDto {
+  @ApiProperty({
+    description: 'Полный URL ссылки, для генерации короткой ссылки',
+    example: 'https://www.google.com',
+  })
   @IsUrl()
   link: string;
 }
--- src/links/dto/get-link.dto.ts
+++ src/links/dto/get-link.dto.ts
@@ -1,6 +1,11 @@
 import { IsAlphanumeric } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetLinkDto {
+  @ApiProperty({
+    description: 'Код короткой ссылки',
+    example: 'j3vveyVa1l',
+  })
   @IsAlphanumeric()
   shortLink: string;
 }
