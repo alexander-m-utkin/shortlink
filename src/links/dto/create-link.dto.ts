95d7ddb8157f1b881521d6b9a35bbce88976b9e5
am_utkin <am_utkin@controlsystems.ru>
2022-12-04 20:44:30 +0300

    text

--- src/links/dto/create-link.dto.ts
+++ src/links/dto/create-link.dto.ts
@@ -3,7 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateLinkDto {
   @ApiProperty({
-    description: 'Полный URL ссылки, для генерации короткой ссылки',
+    description: 'Полный URL ссылки для генерации короткой ссылки',
     example: 'https://www.google.com',
   })
   @IsUrl()
